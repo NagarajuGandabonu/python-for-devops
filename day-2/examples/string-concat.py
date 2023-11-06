be5897fc6c6bf8a1fa619602f8e7eb1eba97921c
Nagaraju-Gandabonu <143990978+NagarajuGandabonu@users.noreply.github.com>
2023-11-06 09:27:20 +0000

    this is my first commit

---
+++ day-2/examples/string-concat.py
@@ -0,0 +1,8 @@
+str1 = "hello"
+str2 = "world"
+str3 = "welcome"
+str4 = "to"
+str5 = "nagaraju"
+str6 = "home"
+result = str1+ " " + str2+ " " + str3+ " " + str4+ " " + str5+ " " + str6
+print(result) 
